2bd22315f8690525269e234184529d1857c95831
unknown <Rifkibachmid58@gmail.com>
2012-04-22 15:25:31 +0700

    core business application commit

---
+++ apps/views/contact_us.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>Buku Import Computer</title>
+		<meta name="description" content="Buku Import Komputer Terbesar Di Inonesia" />
+		<meta name="author" content="rifki" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="../assets/images/favicon2.ico" />
+		<!-- <link rel="apple-touch-icon" href="apps/assets/images/favicon.png" /> -->
+		<link rel="stylesheet" type="text/css" href="../assets/css/depan_statis.css" />
+		<script src="../assets/js/jquery-1.6.4.js"></script>
+		<!--[if lt IE 9]>
+		<script src="apps/assets/js/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<!-- start header -->
+		<header>
+			<p class="title"><b>Buku Import Komputer Nomor 1 Di Indonesia...!</b></p>
+			<a href="#Product.php"><img class="icon_book_store" src="../assets/images/icon_book2.png" title="Book Store" /></a>
+				<a href="../../index.php">Home</a> |
+				<a href="about_us.php">About Us</a> | 
+				<a href="contact_us.php">Contact Us</a> | 
+				<a href="order.php">Order</a> | 
+				<a href="how_to_buy.php">How To Buy</a>
+			
+		</header>
+		<!-- end header -->	
+		<!-- start nav-->
+			<nav></nav>
+		<!-- end nav -->	
+				<aside>
+			  			<div class="sidebar_1">
+			  				<p>Our Category &raquo;</p>
+				  				<ul>
+					  				&raquo; <a href="#category_database.php">Database</a><br />
+									&raquo; <a href="#category_games.php">Games Development</a><br />
+									&raquo; <a href="#category_mobile.php">Mobile Development</a><br />
+									&raquo; <a href="#category_web.php">Web Development</a><br />
+								</ul>
+			  			</div>
+			  			<div class="sidebar_2">
+			  				<p>Our Partner &raquo;</p>
+				  				<ul>
+				  					&raquo; <a href="http://www.unikom.ac.id/" target="_blank">Website Unikom</a><br />
+					  				&raquo; <a href="http://blog.unikom.ac.id/" target="_blank">Blog Unikom</a><br />
+									&raquo; <a href="http://elib.unikom.ac.id/" target="_blank">Digital Library</a><br />
+									&raquo; <a href="http://kuliahonline.unikom.ac.id/" target="_blank">Kuliah Online</a><br />
+									&raquo; <a href="http://repository.unikom.ac.id/" target="_blank">Repository</a><br />
+								</ul>
+			  			</div>
+			  	</aside>
+			<!-- start section -->
+			<section>
+				<p>Contact Us</p>
+			  		<article>
+			  			<form name="form_contact">
+			  				<table border="0">
+			  					<tr>
+			  						<td>Nama  :</td>
+			  						<td><input type="text" name="nama" placeholder="Masukan Nama Anda" id="form_custom_contact_text" required /></td>
+			  					</tr>
+			  					<tr>
+			  						<td>Email :</td>
+			  						<td><input type="email" name="email" placeholder="Masukan Email Anda" id="form_custom_contact_text"/></td>
+			  					</tr>
+			  					<tr>
+			  						<td>Pesan :</td>
+			  						<td><textarea name="pesan" cols="30" rows="5" placeholder="Masukan Pesan Anda Disini" id="form_custom_contact_textarea"/></textarea></td>
+			  					</tr>
+			  					
+			  					<tr>
+			  							<td></td>
+			  							<td>
+			  							<input type="submit" name="submit" value="Submit" id="button_custom_contact"/>
+						  				<input type="reset" name="reset" value="Reset" id="button_custom_contact"/>
+						  				</td>
+			  					</tr>
+					  				
+					  		</table>
+				  		</form>
+			  		</article>
+			</section>
+			<!-- end section -->
+		
+		<!-- start footer -->
+	  <footer>
+			<center><p> <a href="#home">Home</a> | <a href="#home">About Us</a> | <a href="#home">Contact Us</a> | <a href="#home">Product</a></p></center>
+		</footer>
+		<!-- end footer -->
+	</body>
+</html>
